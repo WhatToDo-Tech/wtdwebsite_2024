d77c1c9a63d4a94cf7f6566d2ad918f765eea843
Madalitso Nyemba <40991824+MadalitsoNyemba@users.noreply.github.com>
2024-09-25 22:12:26 +0200

    Added favicon
    Ticket -> https://www.notion.so/Add-favicon-10cb126793f280cea066cae9375c9d94?pvs=4

--- includes/header.php
+++ includes/header.php
@@ -26,20 +26,14 @@
   <link rel="stylesheet" href="css/glightbox.min.css">
   <link rel="stylesheet" href="css/aos.css">
   <link rel="stylesheet" href="css/style.css">
-  <!-- <script nonce="6a6f05ee-d120-4f00-93ec-4719efb09a87">try { (function (w, d) { !function (o, p, q, r) { o[q] = o[q] || {}; o[q].executed = []; o.zaraz = { deferred: [], listeners: [] }; o.zaraz.q = []; o.zaraz._f = function (s) { return async function () { var t = Array.prototype.slice.call(arguments); o.zaraz.q.push({ m: s, a: t }) } }; for (const u of ["track", "set", "debug"]) o.zaraz[u] = o.zaraz._f(u); o.zaraz.init = () => { var v = p.getElementsByTagName(r)[0], w = p.createElement(r), x = p.getElementsByTagName("title")[0]; x && (o[q].t = p.getElementsByTagName("title")[0].text); o[q].x = Math.random(); o[q].w = o.screen.width; o[q].h = o.screen.height; o[q].j = o.innerHeight; o[q].e = o.innerWidth; o[q].l = o.location.href; o[q].r = p.referrer; o[q].k = o.screen.colorDepth; o[q].n = p.characterSet; o[q].o = (new Date).getTimezoneOffset(); if (o.dataLayer) for (const B of Object.entries(Object.entries(dataLayer).reduce(((C, D) => ({ ...C[1], ...D[1] })), {}))) zaraz.set(B[0], B[1], { scope: "page" }); o[q].q = []; for (; o.zaraz.q.length;) { const E = o.zaraz.q.shift(); o[q].q.push(E) } w.defer = !0; for (const F of [localStorage, sessionStorage]) Object.keys(F || {}).filter((H => H.startsWith("_zaraz_"))).forEach((G => { try { o[q]["z_" + G.slice(7)] = JSON.parse(F.getItem(G)) } catch { o[q]["z_" + G.slice(7)] = F.getItem(G) } })); w.referrerPolicy = "origin"; w.src = "../../cdn-cgi/zaraz/sd0d9.js?z=" + btoa(encodeURIComponent(JSON.stringify(o[q]))); v.parentNode.insertBefore(w, v) };["complete", "interactive"].includes(p.readyState) ? zaraz.init() : o.addEventListener("DOMContentLoaded", zaraz.init) }(w, d, "zarazData", "script"); })(window, document) } catch (err) {
-      console.error('Failed to run Cloudflare Zaraz: ', err)
-      fetch('/cdn-cgi/zaraz/t', {
-        credentials: 'include',
-        keepalive: true,
-        method: 'GET',
-      })
-    };</script> -->
+  <link rel="icon" type="image/x-icon" href="./images/w.png">
+  
 </head>
 
 <body>
   <nav class="navbar navbar-expand-lg  ftco-navbar-light">
     <div class="container-xl">
-      <a class="navbar-brand d-flex align-items-center" href="index.php">
+      <a class="navbar-brand d-flex align-items-center" href="index">
         <div class="icon-logo d-flex align-items-center justify-content-center"><span><img src="images/w.png"
               height="60"></span></div>
         <span style="font-weight: 100;"> '''hatToDo Technologies</span>
@@ -50,10 +44,10 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
-          <!-- <li class="nav-item"><a class="nav-link" href="work.php">Services</a></li>
-          <li class="nav-item"><a class="nav-link" href="blog.php">Projects</a></li> -->
-          <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
-          <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+          <!-- <li class="nav-item"><a class="nav-link" href="work">Services</a></li>
+          <li class="nav-item"><a class="nav-link" href="blog">Projects</a></li> -->
+          <li class="nav-item"><a class="nav-link" href="about">About</a></li>
+          <li class="nav-item"><a class="nav-link" href="contact">Contact</a></li>
         </ul>
       </div>
     </div>
